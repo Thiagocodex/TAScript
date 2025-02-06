d0c0dddc27e30832478b69685ea8e21ed5dbbbba
Thiagocodex <thiagoaugustustrabalho@gmail.com>
2025-02-06 13:39:24 +0000

    adicionei um comentario

--- ola_mundo.c
+++ ola_mundo.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(){
+    //esse é so um exemplo de código em C++ para vc visualizar
+    //lembrando que sempre que vc for escrever um código ou criar um arquivo para codificar
+    //vc tem que lembrar de por a estção no final EX: ola_mundo.c, veja que a estanção esta no final
+    //caso vc queira outa linguagem, é so iniciar a criação do arquivo com a extenção correta que A propria IDE vai solicitar que vc instale a extenção 
+    //qualque duvida mandda um zap.
 
     printf("Olá mundo!\n");
     printf(";)\n...");
